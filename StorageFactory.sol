e0c92aba99b26514db2edb4d93970d37e85ff35d
mianjawadahmad <a.jawad321@gmail.com>
2021-09-16 16:00:54 +0500

    Create StorageFactory.sol

---
+++ StorageFactory.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "./SimpleStorage.sol";
+
+contract StorageFactory {
+    
+    SimpleStorage[] public simpleStorageArray;
+    
+    function createSimpleStorageContract() public{
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage); 
+    }
+}
