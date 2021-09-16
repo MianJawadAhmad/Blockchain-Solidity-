58c6ce196015ef6d2494c1cabebf86d1d61116e7
mianjawadahmad <a.jawad321@gmail.com>
2021-09-16 14:30:20 +0500

    Update SimpleStorage.sol

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,3 +1,4 @@
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
 contract SimpleStorage {
@@ -16,11 +17,14 @@ contract SimpleStorage {
     
     People[] public people;
     
+    mapping(string => uint256) public nameToFavoriteNumber;
+    
     function retrieve() public view returns(uint256){
         return favoriteNumber;
     }
     
         function addPerson(string memory _name, uint256 _favoriteNumber) public {
-            people.push(People({favoriteNumber: _favoriteNumber, name:_name}));
+            people.push(People( _favoriteNumber,_name));
+            nameToFavoriteNumber[_name] = _favoriteNumber;
         }
 }
