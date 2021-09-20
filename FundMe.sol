d4ebace006911ce700c0a51e2f00cc8cfc3bb1bf
mianjawadahmad <a.jawad321@gmail.com>
2021-09-20 15:04:10 +0500

    Update FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -9,4 +9,11 @@ contract FundMe {
     function fund() public payable {
         addressAmountFunded[msg.sender] += msg.value;
     }
+
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        );
+        return priceFeed.version();
+    }
 }
