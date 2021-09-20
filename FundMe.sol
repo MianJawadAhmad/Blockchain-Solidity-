017fbb0676da214086ec0ca6cd25e76c04a5dd03
mianjawadahmad <a.jawad321@gmail.com>
2021-09-20 14:12:19 +0500

    Update FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -1,6 +1,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.9.0;
 
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
     mapping(address => uint256) public addressAmountFunded;
 
