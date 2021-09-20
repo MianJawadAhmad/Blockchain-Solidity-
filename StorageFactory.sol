d9de274249c7ce33b0a334b9ed023746aec37046
mianjawadahmad <a.jawad321@gmail.com>
2021-09-20 11:56:03 +0500

    u

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -11,4 +11,14 @@ contract StorageFactory {
         SimpleStorage simpleStorage = new SimpleStorage();
         simpleStorageArray.push(simpleStorage); 
     }
+    
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public{
+      SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
+    }
+    
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256){
+        // SimpleStorage simpleStorage = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
+        return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
+        
+    }
 }
