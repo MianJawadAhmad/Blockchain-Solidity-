b1cdcc58070fc386487f02345887f5f625e6af26
mianjawadahmad <a.jawad321@gmail.com>
2021-09-20 13:21:32 +0500

    Update StorageFactory.sol

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -3,22 +3,26 @@ pragma solidity ^0.6.0;
 
 import "./SimpleStorage.sol";
 
-contract StorageFactory {
-    
+contract StorageFactory is SimpleStorage {
     SimpleStorage[] public simpleStorageArray;
-    
-    function createSimpleStorageContract() public{
+
+    function createSimpleStorageContract() public {
         SimpleStorage simpleStorage = new SimpleStorage();
-        simpleStorageArray.push(simpleStorage); 
+        simpleStorageArray.push(simpleStorage);
     }
-    
-    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public{
-      SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber)
+        public
+    {
+        SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(
+            _simpleStorageNumber
+        );
     }
-    
-    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256){
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
         // SimpleStorage simpleStorage = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
-        return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
-        
+        return
+            SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]))
+                .retrieve();
     }
 }
